f73dbbf34e5a7c5486b5e4aeedd9c2fb40c6b818
Aiden Appleby <aiden.appleby3@gmail.com>
2025-06-15 01:27:36 -0400

    remove sentiment handling on get req

--- routers/GetNews.router.go
+++ routers/GetNews.router.go
@@ -1,16 +1,12 @@
 package routers
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/aidenappl/SentimentScraperAPI/db"
 	"github.com/aidenappl/SentimentScraperAPI/query"
 	"github.com/aidenappl/SentimentScraperAPI/responder"
-	"github.com/aidenappl/SentimentScraperAPI/sentiment"
-	"github.com/aidenappl/SentimentScraperAPI/tools"
 	"github.com/gorilla/mux"
 )
 
@@ -40,21 +36,6 @@ func GetNews(w http.ResponseWriter, r *http.Request) {
 	if news == nil {
 		responder.SendErrorWithParams(w, "News not found", http.StatusNotFound, nil, nil)
 		return
-	}
-
-	if news.Sentiment == nil {
-		if news, err = query.CreateEmptySentiment(db.DB, *news.ID); err != nil {
-			log.Println("❌ Failed to create empty sentiment:", err)
-			responder.SendError(w, http.StatusInternalServerError, "Failed to create empty sentiment", err)
-			return
-		}
-
-		sentiment.QueueSentimentProcessing(news)
-	} else {
-		fmt.Println(news.Sentiment.Status.ID)
-		if news.Sentiment.Status.ID != tools.IntP(4) {
-			sentiment.QueueSentimentProcessing(news)
-		}
 	}
 
 	responder.New(w, news)
